0386234af04d8719a105dfa8b8bf07590e961429
浩哥 <2805404245@qq.com>
2020-09-22 17:43:19 +0800

    模板处理数据

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -6,4 +6,9 @@ from django.http import HttpResponse
 
 #定义试图函数
 def index(request):
-    return HttpResponse('OK!')
+    # return HttpResponse('OK!')
+    #准备上下文:定义在字典中(测试数据)
+    context = {'h2':'天天向上'}
+
+    #将上下文交给模板中进行处理,处理后视图响应给客户端
+    return render(request,'Book/index.html',context)
