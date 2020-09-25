7d5c6d73bb5df818e88e95240304e7aa697341d0
浩哥 <2805404245@qq.com>
2020-09-25 19:23:56 +0800

    设置转换器

--- bookmanager03/Http/urls.py
+++ bookmanager03/Http/urls.py
@@ -1,7 +1,27 @@
 from django.urls import path
 
-from Http.views import index
+from Http.views import index,shop,phone
 
+# 定义转换器,实现判断电话号码
+from django.urls import register_converter
+
+
+class MobileConverter:
+    # 判断数据,正则
+    regex = '1[3-9]\d{9}'
+
+    # 验证没有问题,讲数据给视图函数
+    def to_python(self, value):
+        return value
+
+
+# 使用转换器,必须先注册
+#phone转换器注册成功
+register_converter(MobileConverter, 'phone')
 urlpatterns = [
-    path('index/',index)
+    path('index/',index),
+    #对goods_id设置转换器,必须为整形
+    path('<city_id>/<int:goods_id>/',shop),
+    path('<phone:mobile>/',phone)
+
 ]
--- bookmanager03/Http/views.py
+++ bookmanager03/Http/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return HttpResponse('o_o')
+
+#获取路径值
+def shop(request,city_id,goods_id,):
+    print(city_id,goods_id)
+    return HttpResponse('浩哥的店铺')
+
+#获取电话号码
+def phone(request,mobile):
+    print(mobile)
+    return JsonResponse({'phone':mobile})
